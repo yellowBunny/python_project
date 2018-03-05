32c0a8d4b787acf5ac1dfd3dcb4ba2d245a57ff2
yellowBunny <yellow.bunny51@gmail.com>
2018-03-05 13:45:19 +0100

    version 0.35

--- test_kolko.py
+++ test_kolko.py
@@ -12,14 +12,30 @@ class TestKolko(unittest.TestCase):
              6:['X', 'O', 'X', 'X', '', 'O', '', 'X', 'O'], 7:['', 'O', 'X', 'X', 'O', 'X', '', '', 'O']}
         for key in dd:
             self.assertEqual(kolko_pve.AI_moves(dd[key]),key)
+    def test_draw(self):
+        self.assertTrue(kolko_pve.draw(['X', 'O', 'X', 'O', 'X', 'O', 'X', 'X', 'O']))
+        self.assertFalse(kolko_pve.draw(['', 'O', 'X', 'O', 'X', 'O', 'X', 'X', 'O']))
+        self.assertFalse(kolko_pve.draw(['', '', '', '', '', '', '', '', '']))
+
+    def test_winner(self):
+        self.assertTrue(kolko_pve.winner(['X', 'X', 'X', 'O', 'X', 'O', 'X', 'X', 'O']))
+        self.assertTrue(kolko_pve.winner(['X', 'X', '', 'O', 'X', 'O', 'X', 'X', 'O']))
+        self.assertFalse(kolko_pve.winner(['', '', '', '', '', '', '', '', '']))
+        self.assertFalse(kolko_pve.winner(['X', 'X', '', 'O', 'X', 'O', 'X', 'O', 'O']))
+
+# random tests
 
     def test_AI_moves_random(self):
-
         way_to_win = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]]
         best_moves = [4, 0, 8, 2, 6, 7, 3, 1, 5]
 
+# filtring valid boards random chosens boards
+        for tab in l:
+            x = [all(1 if tab[pos] else 0 for pos in arr) for arr in way_to_win]
+            if any(x):
+                l.remove(tab)
+# create dict with valid boards
         d = {tuple(key):[] for key in l if not all(pos for pos in key)}
-
 
         def win_in_row(tab, sign):
             for arr in way_to_win:
@@ -42,24 +58,6 @@ class TestKolko(unittest.TestCase):
         for key in d:
             self.assertIn(kolko_pve.AI_moves(key),d[key])
 
-
-    def test_draw(self):
-        self.assertTrue(kolko_pve.draw(['X', 'O', 'X', 'O', 'X', 'O', 'X', 'X', 'O']))
-        self.assertFalse(kolko_pve.draw(['', 'O', 'X', 'O', 'X', 'O', 'X', 'X', 'O']))
-        self.assertFalse(kolko_pve.draw(['', '', '', '', '', '', '', '', '']))
-
-    def test_winner(self):
-        self.assertTrue(kolko_pve.winner(['X', 'X', 'X', 'O', 'X', 'O', 'X', 'X', 'O']))
-        self.assertTrue(kolko_pve.winner(['X', 'X', '', 'O', 'X', 'O', 'X', 'X', 'O']))
-        self.assertFalse(kolko_pve.winner(['', '', '', '', '', '', '', '', '']))
-        self.assertFalse(kolko_pve.winner(['X', 'X', '', 'O', 'X', 'O', 'X', 'O', 'O']))
-
-    # def test_human(self):
-    #     self.assertEqual(kolko_pve.human(1),1)
-    #     self.assertEqual(kolko_pve.human(8), 8)
-
-    def test_insert_to_tab(self):
-        pass
 
 
 if __name__ == '__main__':
