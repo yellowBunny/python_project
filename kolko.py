a2bea5eaea22d91b61731c62f344e8f78c6678d1
yellowBunny <yellow.bunny51@gmail.com>
2018-03-11 12:36:59 +0100

    poprawione zgodnie z uwagami

--- kolko.py
+++ kolko.py
@@ -1,66 +1,72 @@
 
-def printer(tab):
-    'print table with positions to choose'    
-    print('{}\n{}\n{}'.format(tab[0],tab[1],tab[2]))
+def printer(board):
+    'print board with positions to choose'
+    print('{}\n{}\n{}'.format(board[0], board[1], board[2]))
 
-def instert_to_tab(player,n):
-    '''inserting player to table return update table
-        player - X or O
-        n - position number'''    
-    c=0
-    for arr in tab:
-        for i,pos in enumerate(arr):
-            if n==c:
-                arr[i]=player
-            c += 1
-    return tab
+def instert_to_board(player, player_choice, board):
+    '''inserting player to board return update board '''
+    counter = 0
+    for arr in board:
+        for i, pos in enumerate(arr):
+            if player_choice == counter:
+                arr[i] = 'X' if player == 0 else 'O'
+            counter += 1
+    return board
 
-def winner(tab):    
-    '''check who win. Return True when anybody win.
-    tab - used_nums'''
+def winner(tab,way_to_win):
+    '''check who win. Return True when anybody win.'''
     for arr in way_to_win:
-        if players[0]==tab[arr[0]]==tab[arr[1]]==tab[arr[2]] or players[1]==tab[arr[0]]==tab[arr[1]]==tab[arr[2]]:
+        if (0==tab[arr[0]]==tab[arr[1]]==tab[arr[2]]or
+            1==tab[arr[0]]==tab[arr[1]]==tab[arr[2]]):
             return True
 
-def draw(tab):
-    '''if draw True
-    tab - used_nums'''
-    return all([ele for ele in tab])
+def draw(board):
+    'when all fild are occupied in used_nums return True'
+    return all(board)
+
+def main_func(player, board_with_fields, used_nums, way_to_win):
+    'main function'
+    while True:
+        choice = int(input('{} Choose position od 0-8: '.format('Player "X"' if player == 0 else 'Player "O"')))
+        if 0 <= choice <= 8:
+            # check if choosen position is empty
+            if used_nums[choice] == '':
+                # when is empty replace from '' to X or O
+                used_nums[choice] = player
+                # prints update table
+                printer(instert_to_board(player, choice, board_with_fields))
+                if winner(used_nums, way_to_win):
+                    print('KoniecGry wygrał {}'.format('X' if player == 0 else 'O'))
+                    break
+                if draw(used_nums):
+                    print('KoniecGry REMIS')
+                    break
+                # swith players
+                if player == 0:
+                    player = 1
+                else:
+                    player = 0
+            else:
+                print('field occupied try again')
+        else:
+            print('Wrong chooice try again 0-8')
+
+
 
 # table to view
-tab = [['1','2','3'],
-       ['4','5','6'],
-       ['7','8','9']]
+board_with_fields = [['0','1','2'],
+                    ['3','4','5'],
+                    ['6','7','8']]
 # list with choosen positions
 used_nums = ['','','','','','','','','']
-# players
-players = ['X','O']
 
 way_to_win = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]
-#main
+player = 0
 
 # [0, 1, 2]
 # [3, 4, 5]
 # [6, 7, 8]
 
-player = 0
-while True:
-    choice = int(input('{} Choose position od 1-9: '.format('Player "X"' if player==0 else 'Player "O"' )))
-    if 0<choice<=9:
-#check if choosen position is empty
-        if used_nums[choice-1]=='':
-# when is empty replace from '' to X or O
-            used_nums[choice-1]=players[player]
-# prints update table
-            printer(instert_to_tab(players[player],choice-1))
-            if winner(used_nums):
-                print('KoniecGry wygrał {}'.format(players[player]))
-                break
-            if draw(used_nums):
-                print('KoniecGry REMIS')
-                break
-#swith players
-            player += 1
-            if player > 1: player = 0
-        else: print('field occupied try again')
-    else: print('Wrong chooice try again 1-9')
+#main
+if __name__ == '__main__':
+    main_func(player, board_with_fields, used_nums, way_to_win)
