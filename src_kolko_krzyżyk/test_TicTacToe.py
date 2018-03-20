0834b4225ff781dc9f545541b5fbc727b56fa560
yellowBunny <yellow.bunny51@gmail.com>
2018-03-20 21:38:49 +0100

    add another test - instert_to_board

--- src_kolko_krzyżyk/test_TicTacToe.py
+++ src_kolko_krzyżyk/test_TicTacToe.py
@@ -4,23 +4,31 @@ import TicTacToe_expand
 class TicTacToe_expand_Test(unittest.TestCase):
 
     def test_prepare_board(self):
+        '''test valid numbers of columns and rows in matrix'''
 
         matrix = {3 : [[0,1,2],[3,4,5],[6,7,8]],
                   4 : [[0,1,2,3],[4,5,6,7],[8,9,10,11],[12,13,14,15]] }
 
-        for key,val in matrix.items():
+        for key, val in matrix.items():
             instance = TicTacToe_expand.TicTacToe(key)
-            self.assertEqual(instance.prepare_board(key),val)
+            self.assertEqual(instance.prepare_board(),val)
 
     def test_used_nums(self):
+        '''make sure we have correct list with empty fields'''
 
         matrix = {3 : 3*3 , 4 : 4*4 }
+
         for key,val in matrix.items():
             print(key,val)
             instance = TicTacToe_expand.TicTacToe(key)
-            self.assertEqual(instance.used_nums,['' for loop in range(val)])
+            self.assertEqual(instance.used_nums,['' for _ in range(val)])
 
-
+    def test_insert_to_board(self):
+        '''make sure value errors will be raise'''
+        player = 0
+        player_choce = '12'
+        instance = TicTacToe_expand.TicTacToe(4)
+        self.assertRaises(ValueError, instance.instert_to_board, player, player_choce)
 
 if __name__ == '__main__':
     unittest.main()
