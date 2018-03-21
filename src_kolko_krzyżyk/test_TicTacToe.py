6f31cb3a3cd7afb57818b15e9e135715aeb8f412
yellowBunny <yellow.bunny51@gmail.com>
2018-03-21 21:53:54 +0100

    inserted changes in test_insert_to_board

--- src_kolko_krzyżyk/test_TicTacToe.py
+++ src_kolko_krzyżyk/test_TicTacToe.py
@@ -24,11 +24,17 @@ class TicTacToe_expand_Test(unittest.TestCase):
             self.assertEqual(instance.used_nums,['' for _ in range(val)])
 
     def test_insert_to_board(self):
-        '''make sure value errors will be raise'''
+        '''make sure we have valid value in valid fields '''
         player = 0
-        player_choce = '12'
+        player_choce = 'dwa'
         instance = TicTacToe_expand.TicTacToe(4)
-        self.assertRaises(ValueError, instance.instert_to_board, player, player_choce)
+        self.assertFalse(instance.instert_to_board(player,player_choce))
+        d = {12 : [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], ['X', 13, 14, 15]],
+             10: [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 'X', 11], ['X', 13, 14, 15]],
+             0: [['X', 1, 2, 3], [4, 5, 6, 7], [8, 9, 'X', 11], ['X', 13, 14, 15]]}
+        for key,val in d.items():
+            self.assertEqual(instance.instert_to_board(player,key),val)
+
 
 if __name__ == '__main__':
     unittest.main()
